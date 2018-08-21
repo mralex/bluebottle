7b5990066548a194c4eb886321fe84dbd8ac32da
Alex Roberts <alex@redprocess.com>
2018-08-21 14:41:25 -0700

    Table header style tweaks

--- app/javascript/components/orders/header.js
+++ app/javascript/components/orders/header.js
@@ -4,26 +4,26 @@ import styles from './style.scss';
 
 const OrdersHeader = (props) => (
   <div className={`row ${styles.header}`}>
-    <div className="col-3">
-      Coffee
+    <div className={`col-3 ${styles.header_cell}`}>
+      <h4>Coffee</h4>
     </div>
-    <div className="col-2">
-      Method
+    <div className={`col-2 ${styles.header_cell}`}>
+      <h4>Method</h4>
     </div>
-    <div className="col-1">
-      Number of Cases
+    <div className={`col-1 ${styles.header_cell}`}>
+      <h4>Number of Cases</h4>
     </div>
-    <div className="col-1">
-      Packets per Case
+    <div className={`col-1 ${styles.header_cell}`}>
+      <h4>Packets per Case</h4>
     </div>
-    <div className="col-2">
-      Ship Date
+    <div className={`col-2 ${styles.header_cell}`}>
+      <h4>Ship Date</h4>
     </div>
-    <div className="col-2">
-      Order
+    <div className={`col-2 ${styles.header_cell}`}>
+      <h4>Order</h4>
     </div>
-    <div className="col-1">
-      View
+    <div className={`col-1 ${styles.header_cell}`}>
+      <h4>View</h4>
     </div>
   </div>
 );
