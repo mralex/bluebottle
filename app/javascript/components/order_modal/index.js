34d0b995e181cbfda752c6c2cf8a24ad8b5130fc
Alex Roberts <alex@redprocess.com>
2018-08-21 12:41:22 -0700

    Fixed up pagination

--- app/javascript/components/order_modal/index.js
+++ app/javascript/components/order_modal/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-
+import moment from 'moment';
 import styles from './style.scss';
 
 import brewMethodMap from '../../utils/brew_methods';
 
 
 export default class OrderModal extends React.PureComponent {
+  state = {
+    order: {
+      coffee_id: 0,
+      brew_method: 'aeropress',
+      ship_at: moment().format('MM/DD/YYYY'),
+      case_count: 10,
+      packets_per_case: 25,
+      is_priority: false,
+      notes: ''
+    }
+  };
+
+  componentWillMount() {
+    if (this.props.order) {
+      this.setState({
+        order: {...this.props.order}
+      });
+    } else {
+      let { order } = this.state;
+      order.coffee_id = this.props.coffees[0].id;
+
+      this.setState({ order });
+    }
+  }
+
+  onChange(e) {
+    let { name, value } = e.target;
+
+    this.setState({
+      order: {
+        ...this.state.order,
+        [name]: value
+      }
+    });
+  }
+
   renderCoffees() {
     const { coffees } = this.props;
 
@@ -17,6 +53,7 @@ export default class OrderModal extends React.PureComponent {
   }
 
   handleSubmit() {
+    this.props.onSave(this.state.order);
     this.props.onClose();
   }
 
@@ -27,6 +64,7 @@ export default class OrderModal extends React.PureComponent {
   }
 
   render() {
+    let { order } = this.state;
 
     return (
       <div className={styles.wrapper}>
@@ -37,45 +75,47 @@ export default class OrderModal extends React.PureComponent {
             <p>Instructional text would go here</p>
           </div>
           <div className="row">
-            <div className="col-50">
+            <div className="col-6">
               <label>Coffee</label>*<br/>
-              <select>
+              <select name="coffee_id" value={order.coffee_id} onChange={this.onChange.bind(this)}>
                 { this.renderCoffees()}
               </select>
             </div>
-            <div className="col-50">
+            <div className="col-6">
               <label>Brew Method</label>*<br/>
-              <select>
+              <select name="brew_method" value={order.brew_method} onChange={this.onChange.bind(this)}>
                 { this.renderBrewMethods()}
               </select>
             </div>
           </div>
           <div className="row">
-            <div className="col-50">
+            <div className="col-6">
               <label>Ship Date</label>*<br />
-              <input type="text"/>
+              <input name="ship_at" type="text" value={order.ship_at} onChange={this.onChange.bind(this)}/>
             </div>
-            <div className="col-25">
+            <div className="col-3">
               <label>Number of Cases</label>*<br />
-              <input type="number"/>
+              <input name="case_count" type="number" value={order.case_count} onChange={this.onChange.bind(this)}/>
             </div>
-            <div className="col-25">
+            <div className="col-3">
               <label>Packets per Case</label>*<br />
-              <select>
+              <select name="packets_pet_case" value={order.packets_per_case} onChange={this.onChange.bind(this)}>
                 <option value="25">25</option>
                 <option value="50">50</option>
               </select>
             </div>
           </div>
-          <div>
-            <label>Notes</label><br />
-            <input type="text" />
+          <div className="row">
+            <div className="col-12">
+              <label>Notes</label><br />
+              <input name="notes" type="text" value={order.notes} onChange={this.onChange.bind(this)}/>
+              <div>
+                <input name="is_priority" type="checkbox" value={order.is_priority} onChange={this.onChange.bind(this)}/>
+                <label>Priority</label><br />
+              </div>
+              <button className="btn" onClick={() => this.handleSubmit() }>Submit Work Order</button>
+            </div>
           </div>
-          <div>
-            <input type="checkbox" />
-            <label>Priority</label><br />
-          </div>
-          <button className="btn" onClick={() => this.handleSubmit() }>Submit Work Order</button>
         </div>
       </div>
     )
