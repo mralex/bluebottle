b0ed0f2f5085a0533ea7e3c35f37530bde9bf1f8
Alex Roberts <alex@redprocess.com>
2018-08-20 17:59:06 -0700

    Added ActiveModelSerializers and Kaminari configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :orders, only: [:index, :create, :update]
+  end
+
+  resources :orders, only: [:index, :create, :update]
+
+  root 'orders#index'
 end
